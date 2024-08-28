84147dbe0cc6bb1a6b0498b06203725062f3a655
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-08-28 14:28:47 +0200

    create Vote repository

---
+++ src/main/java/fr/cda/tpcitoyen/Repositories/VoteRepository.java
@@ -0,0 +1,9 @@
+package fr.cda.tpcitoyen.Repositories;
+
+import fr.cda.tpcitoyen.Entities.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+}
