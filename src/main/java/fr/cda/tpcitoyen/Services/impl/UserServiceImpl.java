744f84a5a4af8caf2ca5137cefcec83422683b58
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-08-28 14:29:50 +0200

    replace constructor by Lombok annotation

--- src/main/java/fr/cda/tpcitoyen/Services/impl/UserServiceImpl.java
+++ src/main/java/fr/cda/tpcitoyen/Services/impl/UserServiceImpl.java
@@ -2,15 +2,11 @@ package fr.cda.tpcitoyen.Services.impl;
 
 import fr.cda.tpcitoyen.Repositories.UserRepository;
 import fr.cda.tpcitoyen.Services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
-    private final UserRepository repo;
-
-    public UserServiceImpl(UserRepository repo) {
-        this.repo = repo;
-    }
-
+    private final UserRepository userRepository;
 }
