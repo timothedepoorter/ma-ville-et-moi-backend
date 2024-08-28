75e3937ed4242bb5d21927ad3061b58ce272ef67
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-08-28 11:49:09 +0200

    init

---
+++ src/main/java/fr/cda/tpcitoyen/TpCitoyenApplication.java
@@ -0,0 +1,13 @@
+package fr.cda.tpcitoyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCitoyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpCitoyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/cda/tpcitoyen/TpCitoyenApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cda.tpcitoyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCitoyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
